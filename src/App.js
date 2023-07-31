21d0ee2de5706b5546e9f20f5741841ad3cb854c
cromano <cromano397@gmail.com>
2023-07-31 19:14:01 +0200

    working on nav

--- src/App.js
+++ src/App.js
@@ -15,11 +15,47 @@ const NotFound = () => {
 const App = () => {
   return (
     <Router basename="/movie_2">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Brand Name</Link>
-        <Link to="/">Exchange Rate</Link>
-        <Link to="/chart/:id">Value Chart</Link>
-      </nav>
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <div className="container">
+            <div className="navbar-nav">
+              <Link className="navbar-brand" to="/">Brand Name</Link>
+              <Link className="nav-item nav-link" to="/">Exchange Rate</Link>
+              <Link className="nav-item nav-link" to="/chart/:id">Value Chart</Link>
+            </div>
+          </div>
+        </div>
+      </nav> */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">Container</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExample07">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu" aria-labelledby="dropdown07">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/chart/:id" component={Chart} />

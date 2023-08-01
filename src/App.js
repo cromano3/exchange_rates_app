46fb17c72bbc4a9b32d7af8a9e235925334ddfd8
cromano <cromano397@gmail.com>
2023-08-01 11:07:39 +0200

    navigation bar working well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Navigation from './Navbar';
 import Home from './Home';
 import Chart from './Chart';
 
 import logo from './logo.svg';
 
 import './App.css';
+
 
 const NotFound = () => {
   return <h2>404 Not Found</h2>;
@@ -14,48 +16,11 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router basename="/movie_2">
-      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <div className="container">
-            <div className="navbar-nav">
-              <Link className="navbar-brand" to="/">Brand Name</Link>
-              <Link className="nav-item nav-link" to="/">Exchange Rate</Link>
-              <Link className="nav-item nav-link" to="/chart/:id">Value Chart</Link>
-            </div>
-          </div>
-        </div>
-      </nav> */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand" href="#">Container</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    // <Router basename="/movie_2">
+    <Router>
+      <Navigation>
 
-        <div className="collapse navbar-collapse" id="navbarsExample07">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div className="dropdown-menu" aria-labelledby="dropdown07">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </Navigation>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/chart/:id" component={Chart} />
---
+++ src/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle(event) {
+    let {isOpen} = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
+
+  render ()
+      {
+        const { isOpen } = this.state;
+        return(
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+           <Link className="navbar-brand" to="/">Brand Name</Link>
+            <button className="navbar-toggler" type="button" onClick={this.handleToggle}>
+             <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/" exact>Exchange Rate</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/chart/:id">Value Chart</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+       </nav>
+        )
+    }
+  
+}
+
+export default Navigation;
